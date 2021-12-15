e321b90130caecfd3b87cd872d63f322144fc346
Douglas Sousa Jorge <douglas@pop-os.localdomain>
2021-12-15 17:51:59 -0300

    feat: new animal row

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
+let apostaIniciada = false;
 let apostas = [];
 const animals = [
     'avestruz',
@@ -41,6 +42,8 @@ const animals = [
 ];
 
 client.on('messageCreate', (message) => {
+    const user = userMention(message.author.id);
+
     // filtros
     if (!message.guild) return;
     if (!message.content.startsWith(prefix)) return;
@@ -48,47 +51,131 @@ client.on('messageCreate', (message) => {
     // rotas
     if (message.content.toLocaleLowerCase() === `${prefix}`) {
         message.channel.send("comandos:");
-        message.channel.send("\"jogar\": inicia uma aposta");
+        message.channel.send("\"apostar\": inicia uma aposta");
+        message.channel.send("\"fechar aposta\": fecha uma aposta e realiza o sorteio");
     }
 
-    if (message.content.toLocaleLowerCase() === `${prefix} jogar`) {
-        message.channel.send("Digite o nome do bicho e o valor da aposta que deseja efetuar (ex: \"Cachorro 100\")");
+    if (message.content.toLocaleLowerCase() === `${prefix} apostar`) {
+        message.channel.send("Digite o nome do bicho e o valor da aposta que deseja efetuar (ex: \"!bicho Cachorro 100\")");
         message.channel.send({ files: [bichoImgURL] });
+        apostaIniciada = true;
+    }
 
-        client.on('messageCreate', (message) => {
-            // filtros
-            if (!message.guild) return;
-            // if (!message.content.startsWith(prefix)) return;
+    const aposta = message.content.split(' ');
 
-            const animal = message.content.split(' ')[0].toLocaleLowerCase();
+    if (aposta.length >= 2) {
+        const animal = aposta[1].toLocaleLowerCase();
+        const valor = Number(aposta[2]);
 
-            if (animals.includes(animal)) {
-                const aposta = message.content.split(' ');
-                apostas.push({
-                    animal: aposta[0],
-                    value: aposta[1],
-                    user: message.author.id
-                });
+        if (animals.includes(animal)) {
 
-                const user = userMention(message.author.id);
-
-                message.channel.send(`${user} apostou o valor de ${aposta[1]} no(a) ${aposta[0]}`);
-            } else {
+            if (isNaN(valor)) {
+                message.reply(`${user} aposta inválida`);
                 return;
             }
 
-            console.log(apostas);
-            return;
-        });
+            apostas.push({
+                animal,
+                valor,
+                userId: message.author.id
+            });
 
-        return;
+            message.reply(`${user} apostou R$ ${valor} no(a) ${animal}`);
+
+            console.log(apostas);
+        }
     }
 
     if (message.content.toLocaleLowerCase() === `${prefix} fechar aposta`) {
         const sorteio = Math.trunc(Math.random() * 8999 + 1000);
-        console.log(sorteio);
+
+        const cauda = sorteio % 100;
+
         message.channel.send(`número sorteado: ${sorteio}`);
+
+        if (cauda >= 0 && cauda <= 100) {
+            if (cauda >= 1 && cauda <= 4) {
+                message.channel.send('Avestruz');
+            }
+            if (cauda >= 5 && cauda <= 8) {
+                message.channel.send('Águia');
+            }
+            if (cauda >= 9 && cauda <= 12) {
+                message.channel.send('Burro');
+            }
+            if (cauda >= 13 && cauda <= 16) {
+                message.channel.send('Borboleta');
+            }
+            if (cauda >= 17 && cauda <= 20) {
+                message.channel.send('Cachorro');
+            }
+            if (cauda >= 21 && cauda <= 24) {
+                message.channel.send('Cabra');
+            }
+            if (cauda >= 25 && cauda <= 28) {
+                message.channel.send('Carneiro');
+            }
+            if (cauda >= 29 && cauda <= 32) {
+                message.channel.send('Camelo');
+            }
+            if (cauda >= 33 && cauda <= 36) {
+                message.channel.send('Cobra');
+            }
+            if (cauda >= 37 && cauda <= 40) {
+                message.channel.send('Coelho');
+            }
+            if (cauda >= 41 && cauda <= 44) {
+                message.channel.send('Cavalo');
+            }
+            if (cauda >= 45 && cauda <= 48) {
+                message.channel.send('Elefante');
+            }
+            if (cauda >= 49 && cauda <= 52) {
+                message.channel.send('Galo');
+            }
+            if (cauda >= 53 && cauda <= 56) {
+                message.channel.send('Gato');
+            }
+            if (cauda >= 57 && cauda <= 60) {
+                message.channel.send('Jacaré');
+            }
+            if (cauda >= 61 && cauda <= 64) {
+                message.channel.send('Leão');
+            }
+            if (cauda >= 65 && cauda <= 68) {
+                message.channel.send('Macaco');
+            }
+            if (cauda >= 69 && cauda <= 72) {
+                message.channel.send('Porco');
+            }
+            if (cauda >= 73 && cauda <= 76) {
+                message.channel.send('Pavão');
+            }
+            if (cauda >= 77 && cauda <= 80) {
+                message.channel.send('Peru');
+            }
+            if (cauda >= 81 && cauda <= 84) {
+                message.channel.send('Touro');
+            }
+            if (cauda >= 85 && cauda <= 88) {
+                message.channel.send('Tigre');
+            }
+            if (cauda >= 89 && cauda <= 92) {
+                message.channel.send('Urso');
+            }
+            if (cauda >= 93 && cauda <= 96) {
+                message.channel.send('Veado');
+            }
+            if ((cauda >= 97 && cauda <= 99) || 0) {
+                message.channel.send('Vaca');
+            }
+        }
+
+        console.log(cauda);
+        console.log(sorteio);
+
         apostas = [];
+        apostaIniciada = false;
     }
 });
 
