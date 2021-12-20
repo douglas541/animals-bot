72aa776251e82b336936131ef12832d7ac780ee5
Douglas Sousa Jorge <douglas@pop-os.localdomain>
2021-12-20 18:57:37 -0300

    feat: v1.0

--- src/index.js
+++ src/index.js
@@ -41,6 +41,61 @@ const animals = [
     'vaca'
 ];
 
+const animalRow = (cauda) => {
+    if (cauda >= 0 && cauda <= 100) {
+        if (cauda >= 1 && cauda <= 4)
+            return ('Avestruz');
+        if (cauda >= 5 && cauda <= 8)
+            return ('Águia');
+        if (cauda >= 9 && cauda <= 12)
+            return ('Burro');
+        if (cauda >= 13 && cauda <= 16)
+            return ('Borboleta');
+        if (cauda >= 17 && cauda <= 20)
+            return ('Cachorro');
+        if (cauda >= 21 && cauda <= 24)
+            return ('Cabra');
+        if (cauda >= 25 && cauda <= 28)
+            return ('Carneiro');
+        if (cauda >= 29 && cauda <= 32)
+            return ('Camelo');
+        if (cauda >= 33 && cauda <= 36)
+            return ('Cobra');
+        if (cauda >= 37 && cauda <= 40)
+            return ('Coelho');
+        if (cauda >= 41 && cauda <= 44)
+            return ('Cavalo');
+        if (cauda >= 45 && cauda <= 48)
+            return ('Elefante');
+        if (cauda >= 49 && cauda <= 52)
+            return ('Galo');
+        if (cauda >= 53 && cauda <= 56)
+            return ('Gato');
+        if (cauda >= 57 && cauda <= 60)
+            return ('Jacaré');
+        if (cauda >= 61 && cauda <= 64)
+            return ('Leão');
+        if (cauda >= 65 && cauda <= 68)
+            return ('Macaco');
+        if (cauda >= 69 && cauda <= 72)
+            return ('Porco');
+        if (cauda >= 73 && cauda <= 76)
+            return ('Pavão');
+        if (cauda >= 77 && cauda <= 80)
+            return ('Peru');
+        if (cauda >= 81 && cauda <= 84)
+            return ('Touro');
+        if (cauda >= 85 && cauda <= 88)
+            return ('Tigre');
+        if (cauda >= 89 && cauda <= 92)
+            return ('Urso');
+        if (cauda >= 93 && cauda <= 96)
+            return ('Veado');
+        if ((cauda >= 97 && cauda <= 99) || 0)
+            return ('Vaca');
+    }
+}
+
 client.on('messageCreate', (message) => {
     const user = userMention(message.author.id);
 
@@ -86,96 +141,42 @@ client.on('messageCreate', (message) => {
         }
     }
 
-    if (message.content.toLocaleLowerCase() === `${prefix} fechar aposta`) {
+    if (message.content.toLocaleLowerCase() === `${prefix} fechar aposta` && apostaIniciada) {
+        let apostasTotais = 0, apostasVencedoras = 0;
         const sorteio = Math.trunc(Math.random() * 8999 + 1000);
-
         const cauda = sorteio % 100;
+        const animalSorteado = animalRow(cauda);
+
+        const vencedores = apostas.filter((aposta) => {
+            if (aposta.animal === animalSorteado.toLocaleLowerCase) {
+                return aposta;
+            }
+        });
+
+        apostas.forEach((aposta) => {
+            apostasTotais += Number(aposta.valor);
+        });
+
+        vencedores.forEach((aposta) => {
+            apostasVencedoras += Number(aposta.valor);
+        });
 
         message.channel.send(`número sorteado: ${sorteio}`);
+        message.channel.send(`animal sorteado na cauda: ${animalRow(cauda)}`);
 
-        if (cauda >= 0 && cauda <= 100) {
-            if (cauda >= 1 && cauda <= 4) {
-                message.channel.send('Avestruz');
-            }
-            if (cauda >= 5 && cauda <= 8) {
-                message.channel.send('Águia');
-            }
-            if (cauda >= 9 && cauda <= 12) {
-                message.channel.send('Burro');
-            }
-            if (cauda >= 13 && cauda <= 16) {
-                message.channel.send('Borboleta');
-            }
-            if (cauda >= 17 && cauda <= 20) {
-                message.channel.send('Cachorro');
-            }
-            if (cauda >= 21 && cauda <= 24) {
-                message.channel.send('Cabra');
-            }
-            if (cauda >= 25 && cauda <= 28) {
-                message.channel.send('Carneiro');
-            }
-            if (cauda >= 29 && cauda <= 32) {
-                message.channel.send('Camelo');
-            }
-            if (cauda >= 33 && cauda <= 36) {
-                message.channel.send('Cobra');
-            }
-            if (cauda >= 37 && cauda <= 40) {
-                message.channel.send('Coelho');
-            }
-            if (cauda >= 41 && cauda <= 44) {
-                message.channel.send('Cavalo');
-            }
-            if (cauda >= 45 && cauda <= 48) {
-                message.channel.send('Elefante');
-            }
-            if (cauda >= 49 && cauda <= 52) {
-                message.channel.send('Galo');
-            }
-            if (cauda >= 53 && cauda <= 56) {
-                message.channel.send('Gato');
-            }
-            if (cauda >= 57 && cauda <= 60) {
-                message.channel.send('Jacaré');
-            }
-            if (cauda >= 61 && cauda <= 64) {
-                message.channel.send('Leão');
-            }
-            if (cauda >= 65 && cauda <= 68) {
-                message.channel.send('Macaco');
-            }
-            if (cauda >= 69 && cauda <= 72) {
-                message.channel.send('Porco');
-            }
-            if (cauda >= 73 && cauda <= 76) {
-                message.channel.send('Pavão');
-            }
-            if (cauda >= 77 && cauda <= 80) {
-                message.channel.send('Peru');
-            }
-            if (cauda >= 81 && cauda <= 84) {
-                message.channel.send('Touro');
-            }
-            if (cauda >= 85 && cauda <= 88) {
-                message.channel.send('Tigre');
-            }
-            if (cauda >= 89 && cauda <= 92) {
-                message.channel.send('Urso');
-            }
-            if (cauda >= 93 && cauda <= 96) {
-                message.channel.send('Veado');
-            }
-            if ((cauda >= 97 && cauda <= 99) || 0) {
-                message.channel.send('Vaca');
-            }
+        if (vencedores.length === 0) {
+            message.channel.send(`Não houve ganhador da aposta!`);
+        } else {
+            vencedores.forEach((aposta) => {
+                const ganho = apostasTotais * (Number(aposta.valor) / apostasVencedoras);
+                message.reply(`${user} ganhou R$ ${ganho}`);
+            });
         }
-
-        console.log(cauda);
-        console.log(sorteio);
-
+        
         apostas = [];
         apostaIniciada = false;
+        apostasTotais = 0;
+        apostasVencedoras = 0;
     }
 });
 
